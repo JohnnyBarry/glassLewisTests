695cc6d10525dad89ba8f9f0610c45dbe9712c41
unknown <jonathanbarry1@gmail.com>
2021-11-22 10:20:45 +0000

    Added readme instructuions and return types in HomePageUtil

--- cypress/Utilities/HomePageUtil.ts
+++ cypress/Utilities/HomePageUtil.ts
@@ -1,13 +1,13 @@
-const visitPage = () => {
-    cy.visit(Cypress.env('home_url'));
-  };
+const visitPage = (): Cypress.Chainable<Cypress.AUTWindow> => {
+  return cy.visit(Cypress.env('home_url'));
+};
 
-const assertPageUrl = () => {
-    cy.url().should('eq', Cypress.env('home_url'));
-  };
+const assertPageUrl = (): Cypress.Chainable<string> => {
+  return cy.url().should('eq', Cypress.env('home_url'));
+};
 
-const assertPageTitle = () => {
-    cy.title().should('eq', Cypress.env('home_page_title'));
-  };
+const assertPageTitle = (): Cypress.Chainable<string> => {
+  return cy.title().should('eq', Cypress.env('home_page_title'));
+};
 
-export default {visitPage, assertPageUrl, assertPageTitle}
+export default { visitPage, assertPageUrl, assertPageTitle };
